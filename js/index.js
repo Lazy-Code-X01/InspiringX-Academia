955a0ff3afbf800d98408d8b77ab92633c0f243f
unknown <lazycode686@gmail.com>
2023-03-22 09:26:09 -0700

    academy 1.0

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// grabbing the button to scroll down to register Section
+const getStarted = document.querySelector('.btn')
+
+// onclick event
+getStarted.addEventListener('click', () => {
+    location.href = '#reg'
+})
